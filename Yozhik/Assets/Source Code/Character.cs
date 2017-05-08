1fb06624d414bfc100a16e3f6b9746abf0630d48
Red-Cat-Fat <boronnikov_a_i@mail.ru>
2017-05-09 00:17:46 +0400

    "ArgumentException: Input Axis Horizontal is not setup"

--- Yozhik/Assets/Source Code/Character.cs
+++ Yozhik/Assets/Source Code/Character.cs
@@ -5,17 +5,41 @@ public class Character : MonoBehaviour {
 
 	[SerializeField]
 	public float speed = 3f;
-	public float jumpValue = 15f;
+    public float jumpForce = 15f;
+    public float jumpValue = 15f;
 
-	new private Rigidbody2D rigidbody;
+    new private Rigidbody2D rigidbody;
 	private Animator animator;
-	// Use this for initialization
-	void Start () {
+    private SpriteRenderer sprite;
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    // Use this for initialization
+    private void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+    // Update is called once per frame
+    private void Update () {
+        if (Input.GetButton("Horisontal")) Run();
+        if (Input.GetButton("Jump")) Jump();
 	}
+
+    private void Run()
+    {
+        Vector3 direction = transform.right * Input.GetAxis("Horisontal");
+
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
+        sprite.flipX = direction.x < 0;
+    }
+
+    private void Jump()
+    {
+        rigidbody.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+    }
 }
