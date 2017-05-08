06a9a29ab4690c68e0dbaec44b6756dde65e59fb
Red-Cat-Fat <boronnikov_a_i@mail.ru>
2017-05-08 23:48:02 +0400

    Create background

---
+++ Yozhik/Assets/Source Code/Character.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+
+	[SerializeField]
+	public float speed = 3f;
+	public float jumpValue = 15f;
+
+	new private Rigidbody2D rigidbody;
+	private Animator animator;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
